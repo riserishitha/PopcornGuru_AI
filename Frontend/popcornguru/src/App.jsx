6a717b0a590a0ed9dfc941ea749397f8e3d2d767
riserishitha <rishitha.n@kalvium.community>
2024-11-12 21:54:55 +0530

    create react app

---
+++ Frontend/popcornguru/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1>POPCORNGURU - AI🍿</h1>
+      <img src="https://compote.slate.com/images/b6fcede3-4025-454e-b32c-e722afdf1ba1.jpg" height='300px' />
+      <h4>Welcome to PopcornGuru! 
+        <br />
+        Your ultimate AI companion for personalized movie recommendations. 
+        <br />
+        Simply select your preferred genre and language, and I will whip up a list of films that will make your next movie night unforgettable. 
+        <br />
+        Get ready to discover your perfect watch, handpicked just for you!</h4>
+
+    </>
+  )
+}
+
+export default App
