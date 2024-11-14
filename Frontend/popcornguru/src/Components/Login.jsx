1edfd51eb7cb292e4c48dc636f0e0ba7812929d3
KarthikaMovva <karthikamovva8306@gmail.com>
2024-11-14 23:26:45 +0530

    initialized login page

---
+++ Frontend/popcornguru/src/Components/Login.jsx
@@ -0,0 +1,10 @@
+function Login(){
+    return(
+        <div>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="Password"/>
+        </div>
+    )
+
+}
+export default Login
